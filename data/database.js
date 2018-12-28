dde39daf0f1363423faea8f595c0bba5f955043f
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2018-12-28 14:48:16 +0700

    2 -> 4 space

--- data/database.js
+++ data/database.js
@@ -1,5 +1,5 @@
 var Datastore = require("nedb"),
-  db = new Datastore({ autoload: true });
+    db = new Datastore({ autoload: true });
 
 db = {};
 db.users = new Datastore("data/users.db");
@@ -9,115 +9,116 @@ db.users.loadDatabase();
 db.submissions.loadDatabase();
 
 function usernameChecking(username) {
-  for (let i = 0; i < username.length; i++) {
-    var c = username[i];
-    if (
-      !(
-        ("0" <= c && c <= "9") ||
-        ("a" <= c && c <= "z") ||
-        ("A" <= c && c <= "Z") ||
-        c == "_" ||
-        c == "-"
-      )
-    )
-      return false;
-  }
-  return true;
+    for (let i = 0; i < username.length; i++) {
+        var c = username[i];
+        if (
+            !(
+                ("0" <= c && c <= "9") ||
+                ("a" <= c && c <= "z") ||
+                ("A" <= c && c <= "Z") ||
+                c == "_" ||
+                c == "-"
+            )
+        )
+            return false;
+    }
+    return true;
 }
 
 export function newUser(username, pass) {
-  return new Promise((resolve, reject) => {
-    db.users.findOne({ username: username }, function(err, docs) {
-      if (err) reject(err);
-      else if (username.length > 32)
-        reject("this username's length is too long");
-      else if (pass.length > 32) reject("this password's length is too long");
-      else if (usernameChecking(username) === false)
-        reject("this username included invalid characters");
-      else if (docs !== null) reject("this username has been taken");
-      else
-        db.users.insert([{ username: username, pass: pass }], function(
-          err2,
-          docs2
-        ) {
-          if (err2) reject(err2);
-          else resolve("user created");
+    return new Promise((resolve, reject) => {
+        db.users.findOne({ username: username }, function(err, docs) {
+            if (err) reject(err);
+            else if (username.length > 32)
+                reject("this username's length is too long");
+            else if (pass.length > 32)
+                reject("this password's length is too long");
+            else if (usernameChecking(username) === false)
+                reject("this username included invalid characters");
+            else if (docs !== null) reject("this username has been taken");
+            else
+                db.users.insert([{ username: username, pass: pass }], function(
+                    err2,
+                    docs2
+                ) {
+                    if (err2) reject(err2);
+                    else resolve("user created");
+                });
         });
     });
-  });
 }
 
 export function readUser(username) {
-  return new Promise((resolve, reject) => {
-    db.users.findOne({ username: username }, function(err, docs) {
-      if (err) reject(err);
-      else if (docs === null) reject("invalid username");
-      else resolve(docs);
+    return new Promise((resolve, reject) => {
+        db.users.findOne({ username: username }, function(err, docs) {
+            if (err) reject(err);
+            else if (docs === null) reject("invalid username");
+            else resolve(docs);
+        });
     });
-  });
 }
 
 export function readSubmission(sub_id) {
-  return new Promise((resolve, reject) => {
-    db.submissions.findOne({ _id: sub_id }, function(err, docs) {
-      if (err) reject(err);
-      else if (docs === null) reject("invalid ID");
-      else resolve(docs);
+    return new Promise((resolve, reject) => {
+        db.submissions.findOne({ _id: sub_id }, function(err, docs) {
+            if (err) reject(err);
+            else if (docs === null) reject("invalid ID");
+            else resolve(docs);
+        });
     });
-  });
 }
 
 export function submitCode(source_code, username, problemID) {
-  return new Promise((resolve, reject) => {
-    db.users.findOne({ username: username }, function(err, docs) {
-      if (err) reject(err);
-      else if (docs === null) reject("this username doesn't exists");
-      else
-        db.submissions.insert(
-          [
-            {
-              source_code: source_code,
-              status: "pending",
-              date: new Date(),
-              username: username,
-              problemID: problemID
-            }
-          ],
-          function(err2, docs2) {
-            if (err2) reject(err2);
-            else resolve("submitted");
-          }
-        );
+    return new Promise((resolve, reject) => {
+        db.users.findOne({ username: username }, function(err, docs) {
+            if (err) reject(err);
+            else if (docs === null) reject("this username doesn't exists");
+            else
+                db.submissions.insert(
+                    [
+                        {
+                            source_code: source_code,
+                            status: "pending",
+                            date: new Date(),
+                            username: username,
+                            problemID: problemID
+                        }
+                    ],
+                    function(err2, docs2) {
+                        if (err2) reject(err2);
+                        else resolve("submitted");
+                    }
+                );
+        });
     });
-  });
 }
 
 export function updateSubmission(sub_id, new_verdict) {
-  return new Promise((resolve, reject) => {
-    db.submissions.update(
-      { _id: sub_id },
-      { $set: { status: new_verdict } },
-      { multi: false },
-      function(err, docs) {
-        if (err) reject(err);
-        else if (docs === 0) reject("nothing was submitted");
-        else resolve("new verdict applied");
-      }
-    );
-  });
+    return new Promise((resolve, reject) => {
+        db.submissions.update(
+            { _id: sub_id },
+            { $set: { status: new_verdict } },
+            { multi: false },
+            function(err, docs) {
+                if (err) reject(err);
+                else if (docs === 0) reject("nothing was submitted");
+                else resolve("new verdict applied");
+            }
+        );
+    });
 }
 
 export function updateUser(username, old_pass, new_pass) {
-  return new Promise((resolve, reject) => {
-    db.users.update(
-      { username: username, pass: old_pass },
-      { $set: { pass: new_pass } },
-      { multi: false },
-      function(err, docs) {
-        if (err) reject(err);
-        else if (docs === 0) reject("wrong password or username");
-        else resolve("password changed");
-      }
-    );
-  });
+    return new Promise((resolve, reject) => {
+        db.users.update(
+            { username: username, pass: old_pass },
+            { $set: { pass: new_pass } },
+            { multi: false },
+            function(err, docs) {
+                if (err) reject(err);
+                else if (docs === 0) reject("wrong password or username");
+                else resolve("password changed");
+            }
+        );
+    });
 }
--- test/database_test.js
+++ test/database_test.js
@@ -1,10 +1,10 @@
 import {
-  newUser,
-  readUser,
-  submitCode,
-  readSubmission,
-  updateUser,
-  updateSubmission
+    newUser,
+    readUser,
+    submitCode,
+    readSubmission,
+    updateUser,
+    updateSubmission
 } from "../data/database";
 
 const sampleUser = "sampleuser";
@@ -15,37 +15,37 @@ const sampleID = "sampleID";
 const sampleVer = "sampleverdict";
 
 describe("newUser", function() {
-  it("should be adding user", function() {
-    return newUser(sampleUser, samplePass);
-  });
+    it("should be adding user", function() {
+        return newUser(sampleUser, samplePass);
+    });
 });
 
 describe("readUser", function() {
-  it("should be returning an object represents user's properties", function() {
-    return readUser(sampleUser);
-  });
+    it("should be returning an object represents user's properties", function() {
+        return readUser(sampleUser);
+    });
 });
 
 describe("submitCode", function() {
-  it("should be submitting code", function() {
-    return submitCode(sampleCode, sampleUser, sampleProb);
-  });
+    it("should be submitting code", function() {
+        return submitCode(sampleCode, sampleUser, sampleProb);
+    });
 });
 
 describe("readSubmission", function() {
-  it("should be returning an object represents submission's props", function() {
-    return readSubmission(sampleID);
-  });
+    it("should be returning an object represents submission's props", function() {
+        return readSubmission(sampleID);
+    });
 });
 
 describe("updateSubmission", function() {
-  it("should be updating verdict", function() {
-    return updateSubmission(sampleID, sampleVer);
-  });
+    it("should be updating verdict", function() {
+        return updateSubmission(sampleID, sampleVer);
+    });
 });
 
 describe("updateUser", function() {
-  it("should be update user's password", function() {
-    return updateUser(sampleUser, samplePass, samplePass);
-  });
+    it("should be update user's password", function() {
+        return updateUser(sampleUser, samplePass, samplePass);
+    });
 });
