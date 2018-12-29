ba39b3804d7b69f55b6aa438be7c466eab0a71ab
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2018-12-29 11:30:25 +0700

    Fix misleading name

--- test/kon_test.js
+++ test/kon_test.js
@@ -1,14 +1,14 @@
-import { check, send, get, clone } from "../driver/themis";
+import Judger from "../driver/kon";
 
-const host = "http://localhost:30000";
+const judger = new Judger("http://localhost:30000");
 
 describe("check", function() {
     it("should return false when server hasn't been set up", function() {
-        return check(host + "/check").then(
+        return judger.check().then(
             () => {
                 throw "Incorrect behaviour";
             },
-            err => {
+            (err) => {
                 if (err === "Server is not ready") return Promise.resolve();
                 else throw err;
             }
@@ -18,39 +18,41 @@ describe("check", function() {
 
 describe("clone", function() {
     it("should return false when clone wrong file", function() {
-        return clone(host + "/task", "./test/LARES.cpp")
-            .then(err => {
+        return judger
+            .clone("./test/LARES.cpp")
+            .then((err) => {
                 throw "Incorrect behaviour";
             })
-            .catch(err => {
+            .catch((err) => {
                 if (err === "Incorrect file type") return Promise.resolve();
                 else throw err;
             });
     });
     it("...and return true when correctly and successfully cloning Task file to judger ...", function() {
-        return clone(host + "/task", "./test/Tasks.zip");
+        return judger.clone("./test/Tasks.zip");
     });
 });
 
 describe("check", function() {
     it("should return true when server is ready", () => {
-        return check(host + "/check");
+        return judger.check("/check");
     });
 });
 
 describe("send", function() {
     it("should return true when sending .cpp ...", () => {
-        return send(host + "/submit", "./test/submitcode.cpp", "hash sha-256");
+        return judger.send("./test/submitcode.cpp", "hash sha-256");
     });
     it("...and return true when sending .pas ...", () => {
-        return send(host + "/submit", "./test/submitcode.pas", "hash sha-256");
+        return judger.send("./test/submitcode.pas", "hash sha-256");
     });
     it("...and false when sending .js", () => {
-        return send(host + "/submit", "./test/submitcode.txt", "hash sha-256")
-            .then(err => {
+        return judger
+            .send("./test/submitcode.txt", "hash sha-256")
+            .then((err) => {
                 throw "Incorrect behaviour";
             })
-            .catch(err => {
+            .catch((err) => {
                 if (err === "Incorrect file type") return Promise.resolve();
                 else throw err;
             });
@@ -60,7 +62,7 @@ describe("send", function() {
 describe("get", function() {
     it("should return true when receiving result, or false otherwise", function() {
         // Unknown
-        return get(host + "/get").then(result => {
+        return judger.get().then((result) => {
             if (!result) throw "Invalid result";
         });
     });
