303242f9a9bc6356ac0191cdbc752435c27a4e60
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2018-12-29 14:40:20 +0700

    Wrap all test into testcase

--- test/database_test.js
+++ test/database_test.js
@@ -14,38 +14,40 @@ const sampleCode = "samplecode";
 const sampleID = "sampleID";
 const sampleVer = "sampleverdict";
 
-describe("newUser", function() {
-    it("should be adding user", function() {
-        return newUser(sampleUser, samplePass);
+describe("database", function() {
+    describe("newUser", function() {
+        it("should be adding user", function() {
+            return newUser(sampleUser, samplePass);
+        });
     });
-});
 
-describe("readUser", function() {
-    it("should be returning an object represents user's properties", function() {
-        return readUser(sampleUser);
+    describe("readUser", function() {
+        it("should be returning an object represents user's properties", function() {
+            return readUser(sampleUser);
+        });
     });
-});
 
-describe("submitCode", function() {
-    it("should be submitting code", function() {
-        return submitCode(sampleCode, sampleUser, sampleProb);
+    describe("submitCode", function() {
+        it("should be submitting code", function() {
+            return submitCode(sampleCode, sampleUser, sampleProb);
+        });
     });
-});
 
-describe("readSubmission", function() {
-    it("should be returning an object represents submission's props", function() {
-        return readSubmission(sampleID);
+    describe("readSubmission", function() {
+        it("should be returning an object represents submission's props", function() {
+            return readSubmission(sampleID);
+        });
     });
-});
 
-describe("updateSubmission", function() {
-    it("should be updating verdict", function() {
-        return updateSubmission(sampleID, sampleVer);
+    describe("updateSubmission", function() {
+        it("should be updating verdict", function() {
+            return updateSubmission(sampleID, sampleVer);
+        });
     });
-});
 
-describe("updateUser", function() {
-    it("should be update user's password", function() {
-        return updateUser(sampleUser, samplePass, samplePass);
+    describe("updateUser", function() {
+        it("should be update user's password", function() {
+            return updateUser(sampleUser, samplePass, samplePass);
+        });
     });
 });
