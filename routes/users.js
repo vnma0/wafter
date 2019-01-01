736adb5f8a3c20fcb23584463de2aa24f2f6865b
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2019-01-01 09:02:16 +0700

    Limit /users/:username route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { readUser, readAllUser } from "../data/database";
+import { auth } from "../middleware/auth";
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
+    // TODO: Hide this route
     readAllUser().then(
         (docs) => {
             res.send(docs);
@@ -14,7 +16,8 @@ router.get("/", (req, res) => {
     );
 });
 
-router.get("/:username", (req, res) => {
+router.get("/:username", auth, (req, res) => {
+    if (req.user.id !== req.params.username) res.sendStatus(403).end();
     readUser(req.params.username).then(
         (docs) => {
             res.send(docs);
