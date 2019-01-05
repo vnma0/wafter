082e62cd2ecf00a5423103564a4d43fe2d1e432b
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2019-01-05 21:32:17 +0700

    Add basic server configuration

---
+++ config/server.js
@@ -0,0 +1,10 @@
+// TODO: Add more configuration options
+export default {
+    displayName: "Wafter - Themis Distributed Server",
+    port: 3000,
+    contest: {
+        // Change this to config contest time
+        startTime: new Date("1970", "01", "01", "00", "00", "00"),
+        endTime: new Date("1970", "01", "01", "00", "00", "10")
+    }
+};
