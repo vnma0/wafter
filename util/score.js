f581af99495e9c9d89ff6500145797f934c9e18a
MegumiTadokoro <cosmology.constant.1915@gmail.com>
2019-01-07 17:26:55 +0700

    Fixing score's penalty calculation in ACM style

--- util/score.js
+++ util/score.js
@@ -5,6 +5,8 @@ import {
     readSubmission
 } from "../data/database";
 
+import server from "../config/server";
+
 /**
  * Calculate final result from judger's result, ACM style
  * @param {JSON} result : judger's result file
@@ -12,30 +14,41 @@ import {
  */
 
 export async function ACM(result) {
-    let lastSub = await readLastSatisfy(result.id);
-    let currSub = await readSubmission(result.id);
+    let lastSub = await readLastSatisfy(result.id),
+        currSub = await readSubmission(result.id),
+        begin_time = server.contest.startTime,
+        curr = currSub.date,
+        curr_diff = curr - begin_time;
 
     let status = {
         score: result.finalScore
     };
 
     if (lastSub === {}) {
-        status.penalty = currSub.date;
+        status.penalty = curr_diff;
         return status;
     }
+
+    let last = lastSub.date,
+        last_diff = last - begin_time;
+
+    last_diff -= last_diff % 60000;
+    last_diff /= 60000;
+    curr_diff -= curr_diff % 60000;
+    curr_diff /= 60000;
 
     let cnt1 = countToSatisfy(result.id) * 20,
         cnt2 = countToSatisfy(lastSub._id) * 20;
 
     if (result.status === "AC") {
         if (lastSub.status === "AC")
-            status.penalty = Math.min(currSub.date + cnt1, lastSub.date + cnt2);
+            status.penalty = Math.min(curr_diff + cnt1, last_diff + cnt2);
         else {
-            status.penalty = currSub.date + cnt1;
+            status.penalty = curr_diff + cnt1;
         }
     } else {
-        if (lastSub.status === "AC") status.penalty = lastSub.date + cnt2;
-        else status.penalty = Math.max(currSub.date, lastSub.date);
+        if (lastSub.status === "AC") status.penalty = last_diff + cnt2;
+        else status.penalty = Math.max(curr_diff, last_diff);
     }
 
     return status;
