f063790e1c6f5c1052a820396dd191b2e79c4e42
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2019-01-09 19:07:13 +0700

    Move contest details outside

--- config/server.js
+++ config/server.js
@@ -1,12 +1,29 @@
+import { readFileSync } from "fs";
+
 // TODO: Add more configuration options
 // TODO: Import from .env
+
+const contestConfig = "contest.json";
+
+let contestObj = {};
+let name, startTime, endTime;
+
+try {
+    contestObj = JSON.parse(readFileSync(contestConfig));
+    ({ name, startTime, endTime } = contestObj);
+    if (!Array.isArray(startTime) && !Array.isArray(endTime)) throw new Error();
+} catch (err) {
+    throw new Error("Invalid contest file. See contest.sample.json");
+}
+
 export default {
     displayName: "Wafter - Themis Distributed Server",
     port: 3000,
     secret: process.env.SECRET || "UUIDGoesHere",
     contest: {
         // Change this to config contest time
-        startTime: new Date(1970, 0, 1, 0, 0),
-        endTime: new Date(1970, 0, 1, 1, 0)
+        name,
+        startTime: new Date(...startTime),
+        endTime: new Date(...endTime)
     }
 };
