769e1e5931494ca161c4f8b9bdafdef9d1f074db
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2019-01-12 10:01:20 +0700

    (Barely) Filter string Will use got

--- config/kon.js
+++ config/kon.js
@@ -1,6 +1,8 @@
 import { readFileSync, existsSync, writeFileSync } from "fs";
 import { join } from "path";
 
+import validUrl from "valid-url";
+
 import { cwd } from "./cwd";
 import Judger from "../driver/kon";
 
@@ -11,9 +13,13 @@ const taskFile = join(cwd, "Tasks.zip");
 // Setup konList
 if (!existsSync(konList)) writeFileSync(konList, "");
 
+// TODO: Validate
 const serverList = readFileSync(konList, "utf8")
     .split(/\r\n|\n|\r/)
+    .filter((s) => validUrl.isWebUri(s))
     .map((kon) => new Judger(kon));
+
+console.log(serverList);
 
 export default {
     judgers: serverList,
