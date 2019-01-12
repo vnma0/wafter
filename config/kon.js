603217e22405ce80d30b8778707a50799f5f3fdf
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2019-01-12 09:34:33 +0700

    Fix undefined .konlist

--- config/kon.js
+++ config/kon.js
@@ -1,17 +1,18 @@
-import Judger from "../driver/kon";
 import { readFileSync, existsSync, writeFileSync } from "fs";
 import { join } from "path";
+
 import { cwd } from "./cwd";
+import Judger from "../driver/kon";
 
 const konList = join(cwd, ".konlist");
 
-const taskFile = join(cwd, "Tasks.zip")
+const taskFile = join(cwd, "Tasks.zip");
 
 // Setup konList
-if (!existsSync(konList)) writeFileSync(konList);
+if (!existsSync(konList)) writeFileSync(konList, "");
 
 const serverList = readFileSync(konList, "utf8")
-    .split("\r\n")
+    .split(/\r\n|\n|\r/)
     .map((kon) => new Judger(kon));
 
 export default {
