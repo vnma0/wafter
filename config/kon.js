6fb5b6fa0be975a0d3ec6fa03aa73ddbdf5ca9c8
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2019-01-13 19:21:36 +0700

    Change kon data file to `kon.json`

--- config/kon.js
+++ config/kon.js
@@ -1,4 +1,4 @@
-import { readFileSync, existsSync, writeFileSync, mkdir } from "fs";
+import { readFileSync, existsSync, writeFileSync, mkdirSync } from "fs";
 import { join } from "path";
 
 import validUrl from "valid-url";
@@ -6,22 +6,25 @@ import validUrl from "valid-url";
 import { cwd } from "./cwd";
 import Judger from "../driver/kon";
 
-const konList = join(cwd, ".konlist");
+const konList = join(cwd, "kon.json");
 
 // Require valid folder to work
 const taskFolder = join(cwd, "Tasks");
-mkdir(taskFolder, (err) => {
-    if (err) throw new Error("Cannot access task folder");
-});
+
+if (!existsSync(konList)) mkdirSync(taskFolder);
 
 // Setup konList
-if (!existsSync(konList)) writeFileSync(konList, "");
+if (!existsSync(konList)) writeFileSync(konList, "[]");
 
 // TODO: Validate
-const serverList = readFileSync(konList, "utf8")
-    .split(/\r\n|\n|\r/)
-    .filter((s) => validUrl.isWebUri(s))
-    .map((kon) => new Judger(kon));
+let rawServerList = JSON.parse(readFileSync(konList, "utf8"));
+
+// Try manage error
+if (!Array.isArray(rawServerList)) rawServerList = [rawServerList];
+
+const serverList = rawServerList
+    .filter((s) => validUrl.isWebUri(s.url))
+    .map((kon) => new Judger(kon.url, kon.prob));
 
 export default {
     judgers: serverList,
