2c9d547cae4fa9699ec4a1bf9a965adfeeb836ae
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2019-01-13 08:18:56 +0700

    Update contest.json

--- config/server.js
+++ config/server.js
@@ -12,11 +12,11 @@ if (!existsSync(staticFolder)) mkdirSync(staticFolder);
 export const contestConfig = join(cwd, "contest.json");
 
 let contestObj = {};
-let name, startTime, endTime;
+let name, startTime, endTime, mode;
 
 try {
     contestObj = JSON.parse(readFileSync(contestConfig));
-    ({ name, startTime, endTime } = contestObj);
+    ({ name, startTime, endTime, mode } = contestObj);
     if (!Array.isArray(startTime) && !Array.isArray(endTime)) throw new Error();
 } catch (err) {
     throw new Error("Invalid contest file. See contest.sample.json");
@@ -28,8 +28,9 @@ export default {
     secret: process.env.SECRET || uuidv4(),
     contest: {
         // Change this to config contest time
-        name,
+        name: name,
         startTime: new Date(...startTime),
-        endTime: new Date(...endTime)
+        endTime: new Date(...endTime),
+        mode: mode
     }
 };
