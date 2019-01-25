027a09812e296a7f866979d427248519f3dd82a1
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2019-01-26 06:34:28 +0700

    Change default port

--- src/config/server.js
+++ src/config/server.js
@@ -9,9 +9,12 @@ if (!existsSync(staticFolder)) mkdirSync(staticFolder);
 
 const serverPORT = Number(process.env.PORT);
 
+// TODO: Allow option to be be parsed as parameter in CLI
+// i.e: `--port 3002`
+
 export default {
     displayName: process.env.SERVERNAME || "Wafter - Themis Distributed Server",
-    port: isNaN(serverPORT) ? 3000 : serverPORT,
+    port: isNaN(serverPORT) ? 3001 : serverPORT,
     secret: process.env.SECRET || uuidv4(),
     staticFolder: staticFolder
 };
