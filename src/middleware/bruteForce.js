2f623cc2ee390fd35b258eab77e9d265dad915f2
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2019-01-30 20:30:50 +0700

    Add `./` for brute.db

--- src/middleware/bruteForce.js
+++ src/middleware/bruteForce.js
@@ -2,7 +2,7 @@ import ExpressBrute from "express-brute";
 import ExpressBruteNedbStore from "express-brute-nedb";
 
 const store = new ExpressBruteNedbStore({
-    filename: "brute.db"
+    filename: "./brute.db"
 });
 
 const bruteForce = new ExpressBrute(store);
