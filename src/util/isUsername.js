12d3ef4c170627c1a1ace479bd3f96704f02e7c7
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2019-01-31 19:54:11 +0700

    Put unameCheck outside

---
+++ src/util/isUsername.js
@@ -0,0 +1,24 @@
+/**
+ * Validate username
+ * NOTE: This function is being considered to be replaced
+ * @param {String} username User's name
+ */
+function isUsername(username) {
+    if (username.length > 18 || username.length < 3) return false;
+    for (let i = 0; i < username.length; i++) {
+        let c = username[i];
+        if (
+            !(
+                ("0" <= c && c <= "9") ||
+                ("a" <= c && c <= "z") ||
+                ("A" <= c && c <= "Z") ||
+                c == "_" ||
+                c == "-"
+            )
+        )
+            return false;
+    }
+    return true;
+}
+
+export default isUsername;
