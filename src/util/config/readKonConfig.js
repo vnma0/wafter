d76de964d9b03968506c11881a6760c77681d569
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2019-02-22 19:44:25 +0700

    Seperate parse function in KonConfig

--- src/util/config/readKonConfig.js
+++ src/util/config/readKonConfig.js
@@ -9,24 +9,30 @@ const parseProbList = require("../parseProbList");
 const readConfig = require("../readConfig");
 
 /**
+ * Parse & validate Kon config data into formatted one
+ * @param {Array} configData
+ */
+function parseKonConfig(configData) {
+    if (!Array.isArray(configData)) throw new Error("Invalid Kon config data");
+    return configData
+        .filter((kon) => validUrl.isWebUri(kon.url))
+        .map((kon) => new Judger(kon.url, parseProbList(kon.prob)));
+}
+
+/**
  * Read kon config
  */
 function KonConfig() {
     const konListFile = "kon.json";
     try {
-        // Setup konList
+        // Pre-create konList
         if (!existsSync(konListFile)) writeFileSync(konListFile, "[]");
 
-        let rawServerList = readConfig(konListFile);
-
-        const konList = rawServerList
-            .filter((kon) => validUrl.isWebUri(kon.url))
-            .map((kon) => new Judger(kon.url, parseProbList(kon.prob)));
-
-        return konList;
+        return parseKonConfig(readConfig(konListFile));
     } catch (err) {
         throw new Error(`Failed to read config for Kon: ${err.message}`);
     }
 }
 
 module.exports = KonConfig;
+module.exports.parseKonConfig = parseKonConfig;
