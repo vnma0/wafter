76df3c177c3bb7c7b0a177be36ac2d11d9bec279
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2019-03-02 06:54:08 +0700

    Improve UX

--- src/prompt/contestConfig/time.js
+++ src/prompt/contestConfig/time.js
@@ -25,39 +25,43 @@ const timeList = (() => {
 const offSetList = (() => {
     let now = new Date(0);
     const res = {};
-    for (let i = 1; i <= 20; ++i) {
+    for (let i = 1; i <= 40; ++i) {
         now.setMinutes(now.getMinutes() + 15);
         let str = "";
-        if (now.getHours() > 7)
-            str += (now.getHours() - 7).toString() + " hour(s) ";
-        if (now.getMinutes() !== 0)
-            str += now.getMinutes().toString() + " minute(s)";
+        if (now.getHours() > 7) str += (now.getHours() - 7).toString() + " hr ";
+        if (now.getMinutes() !== 0) str += now.getMinutes().toString() + " min";
         res[str.trim()] = i * 15 * 60 * 1000;
     }
     return res;
 })();
 
-const timePrompt = enquirer.prompt([
-    {
-        type: "select",
-        name: "startTime",
-        message: "Start time:",
-        choices: Object.keys(timeList),
-        limit: 5,
-        result: (val) => timeList[val]
-    },
-    {
-        type: "select",
-        name: "offset",
-        message: "Duration:",
-        choices: Object.keys(offSetList),
-        limit: 5,
-        initial: "1 hour(s)",
-        result: (val) => offSetList[val]
-    }
-]);
+/**
+ * Change time prompt
+ */
+async function timePrompt() {
+    const { startTime, endTime } = enquirer
+        .prompt([
+            {
+                type: "select",
+                name: "startTime",
+                message: "Start time:",
+                choices: Object.keys(timeList),
+                limit: 5,
+                result: (val) => timeList[val]
+            },
+            {
+                type: "select",
+                name: "offset",
+                message: "Duration:",
+                choices: Object.keys(offSetList),
+                limit: 5,
+                initial: "1 hr",
+                result: (val) => offSetList[val]
+            }
+        ])
+        .then((value) => {
+            value.endTime = new Date(value.startTime.getTime() + value.offset);
+        });
+}
 
-module.exports = timePrompt.then((value) => {
-    value.endTime = new Date(value.startTime.getTime() + value.offset);
-    delete value.offset;
-});
+module.exports = timePrompt;
