97d3cc8f6c22f84d5813f7082dc2b9f9f9853e3a
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2019-03-01 22:07:10 +0700

    Rewrite time interface Phew, that's a lot of work

--- src/prompt/contestConfig/time.js
+++ src/prompt/contestConfig/time.js
@@ -4,42 +4,60 @@ const Enquirer = require("enquirer");
 
 const enquirer = new Enquirer();
 
-const validateTime = (timeStr) => {
-    return new Date(timeStr).toJSON() === timeStr;
-};
-
-const getDate = (() => {
+const getNow = () => {
     const now = new Date();
     now.setMilliseconds(0);
     now.setSeconds(0);
     now.setMinutes((Math.ceil(now.getMinutes() / 15) + 1) * 15);
     return now;
+};
+
+const timeList = (() => {
+    let now = getNow();
+    const res = {};
+    for (let i = 0; i < 40; ++i) {
+        res[now.toLocaleString()] = now;
+        now.setMinutes(now.getMinutes() + 15);
+    }
+    return res;
 })();
 
-const prompt = enquirer.prompt({
-    type: "form",
-    name: "contest",
-    message: "Please provide the following information:",
-    choices: [
-        { name: "name", message: "Contest name:" },
-        {
-            name: "startTime",
-            message: "Start time:",
-            initial: getDate.toJSON(),
-            validate: validateTime
-        },
-        {
-            name: "endTime",
-            message: "End time:",
-            initial: getDate.toJSON(),
-            validate: validateTime
-        }
-    ]
-});
+const offSetList = (() => {
+    let now = new Date(0);
+    const res = {};
+    for (let i = 1; i <= 20; ++i) {
+        now.setMinutes(now.getMinutes() + 15);
+        let str = "";
+        if (now.getHours() > 7)
+            str += (now.getHours() - 7).toString() + " hour(s) ";
+        if (now.getMinutes() !== 0)
+            str += now.getMinutes().toString() + " minute(s)";
+        res[str.trim()] = i * 15 * 60 * 1000;
+    }
+    return res;
+})();
 
-prompt
-    .then((value) => {
-        return value;
-    })
-    .then(console.log)
-    .catch(console.error);
+const timePrompt = enquirer.prompt([
+    {
+        type: "select",
+        name: "startTime",
+        message: "Start time:",
+        choices: Object.keys(timeList),
+        limit: 5,
+        result: (val) => timeList[val]
+    },
+    {
+        type: "select",
+        name: "offset",
+        message: "Duration:",
+        choices: Object.keys(offSetList),
+        limit: 5,
+        initial: "1 hour(s)",
+        result: (val) => offSetList[val]
+    }
+]);
+
+module.exports = timePrompt.then((value) => {
+    value.endTime = new Date(value.startTime.getTime() + value.offset);
+    delete value.offset;
+});
