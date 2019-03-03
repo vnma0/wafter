faf0b60143f639cf6dc4c27ce82324912fac742d
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2019-03-03 20:08:09 +0700

    Add ace prompt

---
+++ src/prompt/contestConfig/ace.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const Enquirer = require("enquirer");
+const contestConfig = require("../../util/config/contestConfig");
+
+const enquirer = new Enquirer();
+
+/**
+ * Contest name prompt
+ */
+async function acePrompt() {
+    let _ace = contestConfig.read().allowedCodeExt;
+
+    // Temporary solution: create list of language, then filter non-included
+    const langList = [".C", ".CPP", ".JAVA", ".KT", ".PAS", ".PY"];
+    const initAce = langList.filter((x) => _ace.indexOf(x) > -1);
+    const leftAce = _ace.filter((x) => langList.indexOf(x) === -1);
+    const { ace } = await enquirer.prompt({
+        type: "multiselect",
+        name: "ace",
+        message: "Set allowed code extension",
+        hint: "Use <space> to select, <return> to submit",
+        initial: initAce,
+        choices: [
+            { name: ".C", hint: "- C" },
+            { name: ".CPP", hint: "- C++" },
+            { name: ".JAVA", hint: "- Java" },
+            { name: ".KT", hint: "- Kotlin" },
+            { name: ".PAS", hint: "- Pascal" },
+            { name: ".PY", hint: "- Python" }
+        ]
+    });
+    contestConfig.update({ allowedCodeExt: ace.concat(leftAce) });
+}
+
+module.exports = acePrompt;
+
+acePrompt();
