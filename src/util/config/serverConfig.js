c1ee5b58c99354e8c305bbec42198acca9164681
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2019-03-25 19:34:37 +0700

    Add template config environment

--- src/util/config/serverConfig.js
+++ src/util/config/serverConfig.js
@@ -10,13 +10,30 @@ function serverConfig() {
 
     // TODO: Allow option to be be parsed as parameter in CLI
     // i.e: `--port 3002`
-    const serverPORT = Number(process.env.PORT);
-    return {
-        displayName:
-            process.env.SERVERNAME || "Wafter - Themis Distributed Server",
-        port: isNaN(serverPORT) ? 3001 : serverPORT,
-        secret: process.env.SECRET || uuidv4()
+
+    const templateConfig = {
+        development: {
+            displayName: "Wafter - Development Build",
+            port: 3001,
+            secret: "IloveCookie"
+        },
+        production: {
+            displayName: "Wafter - Themis Distributed Server",
+            port: 80,
+            secret: uuidv4()
+        }
     };
+
+    const config =
+        templateConfig[process.env.NODE_ENV] || templateConfig["production"];
+
+    const finalConfig = {
+        displayName: process.env.SERVERNAME || config.displayName,
+        port: process.env.PORT || config.port,
+        secret: process.env.SECRET || config.secret
+    };
+
+    return finalConfig;
 }
 
 module.exports = serverConfig;
