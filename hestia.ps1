b488a19e27ee0f9112784439f63d5ad340afc05a
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2019-04-20 19:06:33 +0700

    Move script outside so dev can quickly get latest version of hestia

---
+++ hestia.ps1
@@ -0,0 +1,19 @@
+$repo = "vnma0/hestia"
+
+$releases_url = "https://api.github.com/repos/$repo/releases/latest"
+
+Write-Host Determining latest release of Hestia...
+$get_res = Invoke-WebRequest $releases_url | ConvertFrom-Json
+Write-Host Hestia: $get_res.tag_name
+
+Write-Host Downloading latest release...
+$asset_name = $get_res.assets[0].name
+$asset_url = $get_res.assets[0].browser_download_url
+Invoke-WebRequest $asset_url -Out $asset_name
+
+Write-Host Extracting latest release...
+Remove-Item -path .\public -Recurse -Force
+Expand-Archive $asset_name -DestinationPath .\public
+Remove-Item $asset_name
+
+Write-Host Successfully installed Hestia
