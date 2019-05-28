3382cf6671bd27b7d285146bda7562b3e473a896
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2019-05-28 19:27:17 +0700

    Change to more descriptive version

--- src/config/version.js
+++ src/config/version.js
@@ -1,4 +1,4 @@
 module.exports = (() => {
     const meta = require("../../package.json");
-    return meta && meta.version ? meta.version : "Unknown ver.";
+    return meta && meta.version ? meta.version : "Local build";
 })();
