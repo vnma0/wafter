7b8ece0dd98abb8d98b02732a64c729ea0f8bf2f
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2019-06-14 13:17:35 +0700

    Revert change, remove serve-handler

--- src/routes/assets.js
+++ src/routes/assets.js
@@ -2,18 +2,10 @@
 
 const express = require("express");
 
-const handler = require("serve-handler");
-
 const local = require("../config/local");
 
 const router = express.Router();
 
-if (local.active)
-    router.use(async (req, res) => {
-        // req.url is not relative
-        await handler(req, res, {
-            public: local.folder
-        });
-    });
+if (local.active) router.use(express.static(local.folder));
 
 module.exports = router;
