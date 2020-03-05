4fd294a7314b6ed7e7a32fa39f27a856bd8d17d9
Dung Tuan Nguyen <ntddebugger@gmail.com>
2020-03-05 11:58:14 +0700

    Remove unused code

--- src/index.js
+++ src/index.js
@@ -4,26 +4,10 @@ const Console = require("console");
 require("./util/config/contestConfig").genIfNotExist();
 const VERSION = require("./config/version");
 
-// const addUser = require("./prompt/addUser");
-// const contestOptions = require("./prompt/contestOptions");
 const server = require("./server");
+// const contestOptions = require("./prompt/contestOptions");
+// const addUser = require("./prompt/addUser");
 
-// const mainChoices = {
-//     "Contest options": contestOptions,
-// };
-
-/**
- * Main prompt
- */
-// const mainPrompt = new Select({
-//     name: "main",
-//     message: "How can I help you ?",
-//     choices: ["Start server", ...Object.keys(mainChoices), "Version", "Exit"]
-// });
-
-/**
- * Main menu
- */
 function main() {
     process.title = "MIRAI Wafter " + VERSION;
     Console.log("MIRAI Wafter " + VERSION);
@@ -35,29 +19,10 @@ function main() {
         platform: process.platform,
         arch: process.arch
     });
+
+    // TODO: Add OOBE
+
     server();
-    // mainPrompt
-    //     .run()
-    //     .then((res) => {
-    //         if (res === "Start server") {
-                
-    //         } else if (res === "Version") {
-    //             Console.log({
-    //                 wafter: process.versions.wafter,
-    //                 node: process.versions.node,
-    //                 v8: process.versions.v8,
-    //                 platform: process.platform,
-    //                 arch: process.arch
-    //             });
-    //         } else if (Reflect.has(mainChoices, res)) {
-    //             Reflect.apply(mainChoices[res], null, []).catch((err) => {
-    //                 Console.log(
-    //                     `Exited to menu${err.message ? `: ${err.message}` : ""}`
-    //                 );
-    //             });
-    //         }
-    //     })
-    //     .catch(console.error);
 }
 
 main();
