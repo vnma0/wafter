e3ec490f514c5f94453a93af512c046d200b4d18
Dung Tuan Nguyen <ntddebugger@gmail.com>
2020-03-31 20:32:09 +0700

    fix: Remove auto-create index.html script

--- src/config/server.js
+++ src/config/server.js
@@ -1,21 +1,15 @@
 "use strict";
 
 const path = require("path");
-const { existsSync, mkdirSync, writeFileSync } = require("fs");
 const { v4: uuidv4 } = require("uuid");
 
 const serverConfig = require("../util/config/serverConfig");
 
 const staticFolder = path.join(__dirname, "../../public");
-if (!existsSync(staticFolder)) mkdirSync(staticFolder);
-const sampleHTML =
-    "<!DOCTYPE html><html><body>Wafter is running as an API server only</body></html>";
-if (!existsSync(staticFolder + "/index.html"))
-    writeFileSync(staticFolder + "/index.html", sampleHTML);
 
 const serverCfg = Object.assign(serverConfig(), {
     secret: uuidv4(),
-    staticFolder: staticFolder
+    staticFolder
 });
 
 module.exports = serverCfg;
